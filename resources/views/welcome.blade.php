4deba130483a8cd2a51cc1153800a3832f2942a6
FlerkenFury <60496179+FlerkenFury@users.noreply.github.com>
2020-10-22 23:40:05 +0700

    homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,68 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Syncode</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/bootstrap-4.0.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/fontawesome/css/all.min.css">
+</head>
+<body>
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+    <div class="bg">
+        <nav class="navbar justify-content-between">
+            <div class="logo">
+                <a href="#">
+                    <img src = "/assets/logo.png" height="100px">
+                </a>
+            </div>
+            <div class="menu">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">HOME</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">LOGIN</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">REGISTER</a>
                         @endif
                     @endauth
                 </div>
             @endif
+                <button class="btn btn-primary btn-lg icon" onclick="sidebarOpen()">
+                    <i class="fa fa-bars"></i>
+                </button>
+            </div>
+        </nav>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+    <br>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <div class="container-fluid">
+        <div class="d-sm-flex justify-content-around content">
+            <div class="content-left">
+                <br><br><br><br>
+                <h2 class="text-blur"><b>BE PART OF OUR</b></h2>
+                <h1>COMMUNITY</h1>
+                <div class="description">
+                    <p>
+                        Indonesia #1 question and answer site for professional and enthusiast programmers
+                    </p>
                 </div>
+                <br>
+                <a href="{{ route('register') }}" class="btn btn-primary btn-lg">JOIN US</a>
+            </div>
+
+            <div class="image1">
+                <img src="/assets/content-removed.png" alt="" height="450px">
             </div>
         </div>
-    </body>
+    </div>
+
+
+    </div>    
+
+</body>
+<script src="/bootstrap-4.0.0/dist/js/bootstrap.min.js"></script>
 </html>
